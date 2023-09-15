55a2886aa1e6141a33a41d98b63ed2a2ed6d1d74
nabilettihadi <nabilettihadi@gmail.com>
2023-09-15 12:05:19 +0100

    Challenge Boucle

--- challenge5.c
+++ challenge5.c
@@ -8,19 +8,18 @@ void main()
     }while(n<0);
 
     for (i=0;i<n;i++){
-    do{
-        printf("Entrer un nombre (0<nombre<100):\n");
-        scanf("%d",&a);   
-    }while(a<0 && a>100);
-
-        if((a%10)==0)
-        som+=a;
-        if (a>max)
-        max=a;
     
+        printf("Entrer un nombre (0<nombre<100):\n");
+        scanf("%d",&a); 
+        
+        if((a%10)==0 && (a>0 && a<100) )
+        som+=a;       
+        if(a>max && (a%10)==0 && (a>0 && a<100))
+        max=a;
+                
     }
 
     printf("la somme de cette serie est : %d:\n",som);
     printf("le max de cette serie est : %d:\n",max);
-     
+
 }
