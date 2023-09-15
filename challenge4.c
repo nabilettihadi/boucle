dd9c7aeddf4a57c0cd0fec66eaaccb8e09a8c3db
nabilettihadi <nabilettihadi@gmail.com>
2023-09-15 17:19:40 +0100

    Tous les challenges des boucles

--- challenge4.c
+++ challenge4.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int n, inv = 0;
+void main()
+{   int a,n,i,som=0,max=0;
 
-    printf("Entrez un entier : ");
-    scanf("%d", &n);
+    do{ printf("Entrer la taille de la serie:\n");
+        scanf("%d",&n);
+    }while(n<0);
 
-    while (n != 0) {
-        inv = inv * 10 + n % 10;
-        n /= 10;
+    for (i=0;i<n;i++){
+    
+        printf("Entrer un nombre (0<nombre<100):\n");
+        scanf("%d",&a); 
+        
+        if((a%10)==0 && (a>0 && a<100) )
+        som+=a;       
+        if(a>max && (a%10)==0 && (a>0 && a<100))
+        max=a;
+                
     }
 
-    printf("L entier inverse est : %d\n", inv);
+    printf("la somme de cette serie est : %d:\n",som);
+    printf("le max de cette serie est : %d:\n",max);
 
-    return 0;
 }
